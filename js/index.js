8a3c26a059577a27772c2d99fa919d9916158534
Charlotte Jana <charlotte.jana@t-online.de>
2019-08-16 14:09:28 +0200

    function adjustKeys

--- js/index.js
+++ js/index.js
@@ -93,3 +93,70 @@ function rotateKeyboard() {
 }
 
 // css display: https://codeburst.io/common-problems-in-positioning-elements-in-css-best-practices-b03ac54dbdcb
+
+function adjustKeys(direction) {
+    var buttons = document.querySelectorAll(".button");
+
+    switch (direction) {
+        case 1: // default variant 
+            for (var i = 0; i < buttons.length; i++) {
+                buttons[i].style.flexDirection = "column";
+                push = buttons[i].querySelector(".push");
+                if(push !== null){
+                    push.classList.remove("top", "bottom", "left", "right");
+                    push.classList.add("top");
+                }
+                pull = buttons[i].querySelector(".pull");
+                if(pull !== null){
+                    pull.classList.remove("top", "bottom", "left", "right");
+                    pull.classList.add("bottom");
+                }
+            } 
+            break;
+        case 2: 
+            for (var i = 0; i < buttons.length; i++) {
+                buttons[i].style.flexDirection = "row-reverse";
+                push = buttons[i].querySelector(".push");
+                if(push !== null){
+                    push.classList.remove("top", "bottom", "left", "right");
+                    push.classList.add("right");
+                }
+                pull = buttons[i].querySelector(".pull");
+                if(pull !== null){
+                    pull.classList.remove("top", "bottom", "left", "right");
+                    pull.classList.add("left");
+                }
+            }
+            break;
+        case 3:
+            for (var i = 0; i < buttons.length; i++) {
+                buttons[i].style.flexDirection = "column-reverse";
+                push = buttons[i].querySelector(".push");
+                if(push !== null){
+                    push.classList.remove("top", "bottom", "left", "right");
+                    push.classList.add("bottom");
+                }
+                pull = buttons[i].querySelector(".pull");
+                if(pull !== null){
+                    pull.classList.remove("top", "bottom", "left", "right");
+                    pull.classList.add("top");
+                }
+            }
+            break;
+        case 4:
+            for (var i = 0; i < buttons.length; i++) {
+                buttons[i].style.flexDirection = "row";
+                push = buttons[i].querySelector(".push");
+                if(push !== null){
+                    push.classList.remove("top", "bottom", "left", "right");
+                    push.classList.add("left");
+                }
+                pull = buttons[i].querySelector(".pull");
+                if(pull !== null){
+                    pull.classList.remove("top", "bottom", "left", "right");
+                    pull.classList.add("right");
+                }
+            }
+            break;
+    }
+}
